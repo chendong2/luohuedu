a5e75765e8d05069a50a08f5d5d6187fe64d83b6
chendong2 <chendong19901127@126.com>
2016-06-22 20:51:28 +0800

    完善表单页面
    完善表单页面

--- LuoHuEduSys/LuoHuEdu/Module/UserInfo/CourseInfo.js
+++ LuoHuEduSys/LuoHuEdu/Module/UserInfo/CourseInfo.js
@@ -1,62 +1,25 @@
 ﻿
 
 //获取JSON数据并填充到相应表单
-getAllSubject();
-getAllSchool();
+getTheYear();
 ajaxCRUD({
     url: '/WebServices/Admin/Student.asmx/GetAllStudent',
     success: function (data) {
-        //JSON数据填充表单
-        loadDataToForm('ff', data);
-        var bir = $("#txtBirthday").val();
-        bir.replace(/Date\([\d+]+\)/, function (a) { eval('d = new ' + a) });
-        $("#txtBirthday").val(d.getFullYear() + "-" + (d.getMonth() + 1) + "-" + d.getDate());
+        fillData();
     }
 });
 
-//获取全部的免修数据
-function getAllSchool() {
-    $("#sSchool").empty();
-    ajaxCRUD({
-        url: '/WebServices/Parameter/School.asmx/GetAllSchool',
-        async: false,
-        success: function (data) {
-            for (var i = 0; i < data.length; i++) {
-                var value = data[i].toString().split('******');
-                var option = "<option value='" + value[1] + "'>" + value[0] + "</option>";
-                $("#sSchool").append(option);
-            }
-        }
-    });
+
+//获取年份数据
+function getTheYear() {
+    var currentYear = new Date().getFullYear();
+    $("#sTheYear").empty();
+    for (var i = 1; i <= 15; i++) {
+        var data = currentYear - i + "-" + (currentYear - i + 1);
+        var option = "<option value='" + data + "'>" + data + "</option>";
+        $("#sTheYear").append(option);
+    }
 }
 
-function getAllSubject() {
-    $("#sFirstTeaching").empty();
-    $("#sSecondTeaching").empty();
-    var option = "<option value=''>未设定</option>";
-    $("#sFirstTeaching").append(option);
-    $("#sSecondTeaching").append(option);
-    ajaxCRUD({
-        url: '/WebServices/Parameter/Subject.asmx/GetAllSubject',
-        async: false,
-        success: function (data) {
-            for (var i = 0; i < data.length; i++) {
-                var value = data[i].toString().split('******');
-                option = "<option value='" + value[1] + "'>" + value[0] + "</option>";
-                $("#sFirstTeaching").append(option);
-                $("#sSecondTeaching").append(option);
-            }
-        }
-    });
-}
-
-function reset() {
-    resetFormAndClearValidate("ff");
-}
-//end saveDataChangeUserPsw()
-//重置表单并同时清除验证
-function resetFormAndClearValidate(formId) {
-    clearForm(formId);
-    resetForm(formId);
-    clearValidate(formId);
+function fillData() {
 }
