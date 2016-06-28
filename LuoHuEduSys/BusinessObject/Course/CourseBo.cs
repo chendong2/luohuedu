6a12b2d70ece82a2bf992730d66f0223d92c8b52
chendong2 <chendong19901127@126.com>
2016-06-28 22:14:51 +0800

    1
    2

--- LuoHuEduSys/BusinessObject/Course/CourseBo.cs
+++ LuoHuEduSys/BusinessObject/Course/CourseBo.cs
@@ -135,9 +135,5 @@ namespace BusinessObject.Course
         /// </summary>
         public string Name { get; set; }
 
-        public int Sex { get; set; }
-
-        public int Profession { get; set; }
-
     }
 }
