38f3f645fe4ff997d2dc49efa77c60335a023a13
chendong2 <chendong19901127@126.com>
2016-06-28 22:10:00 +0800

    修改service
    修改service

--- LuoHuEduSys/Services/Course/CourseControl/AllCourseService.cs
+++ LuoHuEduSys/Services/Course/CourseControl/AllCourseService.cs
@@ -11,11 +11,22 @@ namespace Services.Course.CourseControl
 {
     public class AllCourseService
     {
-        
-        public CourseBo GetMyColloctCourse()
+
+        public CourseBo GetMyColloctCourse(string theYear, string trainType)
         {
             string userId = Domain.common.UserInfo.GetUserId().ToString();
-            return new CourseBo();
+            var courseBo = new CourseBo { };
+            using (var connection = DataBaseConnection.GetMySqlConnection())
+            {
+                var sqlStr =
+                    @"SELECT  NAME,Sex,Profession,b.Period,TheYear,TrainType FROM tb_student st INNER JOIN 
+(SELECT ct.StudentId,SUM(Period) AS Period,TrainType,TheYear  FROM  tb_coursestudent ct INNER JOIN tb_course co  ON co.id=ct.CourseId 
+GROUP BY ct.StudentId,TrainType,TheYear)b ON st.id=b.studentid WHERE TheYear=@TheYear AND TrainType=@TrainType";
+                courseBo = connection.Query<CourseBo>(sqlStr, new { TheYear = theYear, TrainType = trainType }).FirstOrDefault();
+            }
+
+
+            return  courseBo;
         }
 
     }
